1af2a32edb8e5880fb5f28a743e528c25bfc5e36
liu-hz18 <1456437967@qq.com>
2020-10-10 18:59:44 +0800

    fix: news with image width don't match

--- src/components/NewsItem.vue
+++ src/components/NewsItem.vue
@@ -1,80 +1,96 @@
 <!-- 新闻条目 -->
 <template>
-  <div class="NewsItem">
+<div class="NewsItem">
     <el-card v-if="haveImg">
-    <div class="news-body">
-    <el-row :gatter="20">
-    <el-col :span="19">
-            <NewsInfo v-bind:link="link" :title="title" :content="content" :source="source" :time="time" :keywords="keywords"></NewsInfo>
+        <div class="news-body">
+
+            <el-col :span="16">
+                <NewsInfo v-bind:link="link" :title="title" :content="content" :source="source" :time="time" :keywords="keywords"></NewsInfo>
             </el-col>
             <el-col :span="5">
-            <a v-bind:href="imgurl">
-              <img class="news-img" v-bind:src="imgurl" alt="News Image" />
-            </a>
+                <a v-bind:href="imgurl">
+                    <img class="news-img" v-bind:src="imgurl" alt="News Image" />
+                </a>
             </el-col>
-            </el-row>
-      </div>
+
+        </div>
     </el-card>
 
     <el-card v-if="!haveImg">
-          <NewsInfo v-bind:link="link" :title="title" :content="content" :source="source" :time="time" :keywords="keywords"></NewsInfo>
+        <div class="news-body">
+            <el-col :span="24">
+                <NewsInfo v-bind:link="link" :title="title" :content="content" :source="source" :time="time" :keywords="keywords"></NewsInfo>
+            </el-col>
+        </div>
     </el-card>
-  </div>
+</div>
 </template>
 
 <script>
 import NewsInfo from "./NewsInfo.vue";
 
 export default {
-  name: "NewsItem",
-  components: {
-    NewsInfo,
-  },
-  props: {
-    uid: Number,
-    title: String,
-    imgurl: String,
-    content: String,
-    link: String,
-    source: String,
-    time: String,
-    keywords: Array,
-  },
-  data() {
-    return {
-      
-    };
-  },
-  computed: {
-    haveImg() {
-      return this.imgurl != "";
+    name: "NewsItem",
+    components: {
+        NewsInfo,
     },
-  },
+    props: {
+        uid: Number,
+        title: String,
+        imgurl: String,
+        content: String,
+        link: String,
+        source: String,
+        time: String,
+        keywords: Array,
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+        haveImg() {
+            return this.imgurl != "";
+        },
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
+
 a {
-  color: #42b983;
+    color: #42b983;
 }
+
 li {
-  margin-bottom: 5px;
+    margin-bottom: 5px;
 }
+
 .news-img {
-  position: relative;
-  width: 100%;
-  margin-top: 20%;
-  margin-left: 5%;
+    position: relative;
+    width: 140%;
+    margin-top: 10%;
+    margin-left: 20%;
 }
+
 .NewsItem {
-  margin-bottom: 5px;
+    margin-bottom: 5px;
+}
+
+.news-body {
+    position: relative;
+    margin-left: 0%;
+    margin-right: 0%;
 }
 </style>
